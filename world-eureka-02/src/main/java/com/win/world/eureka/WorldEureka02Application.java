2de8ff70d77af1f2a0bcf2b2ae093ddf747bcfad
zuomengjun <zuomengjun@gomeholdings.com>
2018-10-31 23:51:26 +0800

    eureka集群项目搭建

--- world-eureka-02/src/main/java/com/win/world/eureka/WorldEureka02Application.java
+++ world-eureka-02/src/main/java/com/win/world/eureka/WorldEureka02Application.java
@@ -2,8 +2,10 @@ package com.win.world.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WorldEureka02Application {
 
     public static void main(String[] args) {
