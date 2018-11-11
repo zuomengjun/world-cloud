a1a78fc9ebbb8f0b8fddc6aedf96c444b7740538
zuomengjun <zuomengjun@gomeholdings.com>
2018-11-12 00:49:10 +0800

    修改order

--- world-provider-api/world-provider-order-api/src/main/java/com/win/world/order/entity/Order.java
+++ world-provider-api/world-provider-order-api/src/main/java/com/win/world/order/entity/Order.java
@@ -4,4 +4,25 @@ import java.io.Serializable;
 
 public class Order implements Serializable {
 
+    private static final long serialVersionUID = 7767329212197903995L;
+
+    private String id;
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- world-provider-order/src/main/java/com/win/world/order/controller/OrderController.java
+++ world-provider-order/src/main/java/com/win/world/order/controller/OrderController.java
@@ -1,7 +1,50 @@
 package com.win.world.order.controller;
 
+import com.win.world.order.entity.Order;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
+@RequestMapping("/order")
 public class OrderController {
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("get/{id}")
+    public Order get(@PathVariable String id){
+        Order order = new Order();
+        order.setId(id);
+        order.setName("电视");
+        return order;
+    }
+
+    @GetMapping("getPort")
+    public String getPort(){
+        return port;
+    }
+
+    @GetMapping("getServiceInstances")
+    public Object getServiceInstances(){
+        List<String> services = discoveryClient.getServices();
+        System.out.println(services);
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("world-provider-order");
+        for (ServiceInstance instance : instances) {
+            System.out.println(instance.getServiceId() + ":" + instance.getHost() + ":" + instance.getPort() + ":" + instance.getUri());
+        }
+
+        return discoveryClient;
+    }
 }
