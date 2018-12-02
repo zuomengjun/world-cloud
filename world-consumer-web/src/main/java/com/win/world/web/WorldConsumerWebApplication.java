21cbf4bd969b907ac7ecbc6fd11184234804200e
zuomengjun <zuomengjun@gomeholdings.com>
2018-12-02 20:40:54 +0800

    web消费者开启hystrix监控actuator端点

--- world-consumer-web/src/main/java/com/win/world/web/WorldConsumerWebApplication.java
+++ world-consumer-web/src/main/java/com/win/world/web/WorldConsumerWebApplication.java
@@ -2,10 +2,12 @@ package com.win.world.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.win.world"})
 @EnableFeignClients(basePackages = {"com.win.world"})
+@EnableHystrix
 public class WorldConsumerWebApplication {
 
     public static void main(String[] args) {
